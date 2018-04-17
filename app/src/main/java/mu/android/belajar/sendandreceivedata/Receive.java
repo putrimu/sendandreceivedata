04ad911914b416c503b4d2e6210a6a2b3e2b5833
putrimu <putrimariahulfa@gmail.com>
2018-04-17 18:05:12 +0700

    Initial commit

---
+++ app/src/main/java/mu/android/belajar/sendandreceivedata/Receive.java
@@ -0,0 +1,20 @@
+package mu.android.belajar.sendandreceivedata;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Receive extends AppCompatActivity {
+    TextView tampil;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive);
+        tampil = (TextView) findViewById(R.id.tampilan);
+        Intent i = getIntent();
+        String send = i.getStringExtra("Send");
+
+        tampil.setText(send);
+    }
+}
